7c4fb20e2dc645bccbf41f339ed3e262e2970dcf
engineer <newgalaxy401@gmail.com>
2024-05-03 15:14:39 +0300

    add adaptive design of table ticket

--- resources/views/tabletickets/table.blade.php
+++ resources/views/tabletickets/table.blade.php
@@ -1,7 +1,7 @@
 
 <table class="table">
     <caption class="table__caption">Водопровод</caption>
-    <thead>
+    <thead class="table__thead">
         <tr class="table__row">
             <th class="table__header">№</th>
             <th class="table__header">Время <br> регистрации</th>
@@ -22,16 +22,23 @@
         
             @foreach ($tickets as $rowTickets)
             <tr class="table__row">
-                <td class="table__data">{{ $rowTickets->tapwaterticket_id}}</td>
-                <td class="table__data">{{ $rowTickets->date }} {{ $rowTickets->time }}</td>
-                <td class="table__data">{{ $rowTickets->type }}</td>
-                <td class="table__data">{{ $rowTickets->district }} {{ $rowTickets->city }} {{ $rowTickets->street }} {{ $rowTickets->building }}</td>
-                <td class="table__data">{{ $rowTickets->description }}</td>
-                <td class="table__data">{{ $rowTickets->phone }}</td>
-                <td class="table__data">{{ $rowTickets->cityarea }}</td>
-                <td class="table__data"></td>
-                <td class="table__data"></td>
-                <td class="table__data"></td>
+                <td class="table__data" data-label="Заявка №">{{ $rowTickets->tapwaterticket_id}}</td>
+                <td class="table__data" data-label="Время регистрации:">{{ $rowTickets->date }} {{ $rowTickets->time }}</td>
+                <td class="table__data"data-label="Тип:">{{ $rowTickets->type }}</td>
+                <td class="table__data"data-label="Адрес:">{{ $rowTickets->district }} {{ $rowTickets->city }} {{ $rowTickets->street }} {{ $rowTickets->building }}</td>
+                <td class="table__data"data-label="Событие:">{{ $rowTickets->description }}</td>
+                <td class="table__data"data-label="Телефон:">{{ $rowTickets->phone }}</td>
+                <td class="table__data"data-label="Участок">{{ $rowTickets->cityarea }}</td>
+                <td class="table__data" data-label="Время начала работ"></td>
+                <td class="table__data" data-label="Время окончания работ"></td>
+                <td class="table__data" data-label="Опции">
+                    <a class="table__a" src="#">
+                        <img src="{{ asset('css/assets/button-edit-orange.png') }}" title="редактировать заявку №{{ $rowTickets->tapwaterticket_id  }}" class="table__img-edit">
+                    </a>
+                    <a class="table__a" src="#">
+                        <img src="{{ asset('css/assets/button-done-orange.png') }}" title="закрыть заявку №{{ $rowTickets->tapwaterticket_id  }}" class="table__img-done">
+                    </a>
+                </td>
             </tr>
             @endforeach
     </tbody>
